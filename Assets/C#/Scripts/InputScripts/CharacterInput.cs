045b9ba2a64f01544c35cd389e401d2222885781
Chris Jackson <shadowace248@gmail.com>
2018-05-03 09:08:19 -0600

    fixing stuff

--- Assets/C#/Scripts/InputScripts/CharacterInput.cs
+++ Assets/C#/Scripts/InputScripts/CharacterInput.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System;
 
 namespace Assets.Scripts.InputScripts
@@ -9,8 +9,65 @@ namespace Assets.Scripts.InputScripts
     {
 
         public float runSpeed;
-        public float jumpSpeed;
-        private float distToGround;
+        public float jumpSpeed = 7;
+        private float currentJumpPosition;
+        private float jumpFrames = 50;
+        private bool isCrouching = false;
+        private float groundedTolerance = (float)0.5;
+        public KeyCode leftKey = KeyCode.A;
+        public KeyCode upKey = KeyCode.W;
+        public KeyCode downKey = KeyCode.S;
+        public KeyCode rightKey = KeyCode.D;
+        public KeyCode specialKey = KeyCode.LeftControl;
+        public KeyCode attackKey = KeyCode.LeftShift;
+        public bool IsSpecialAttack
+        {
+            get
+            {
+                return animator.GetBool("IsSpecialAttack");
+            }
+            set
+            {
+                if (IsWalking != value)
+                    animator.SetBool("IsSpecialAttack", value);
+            }
+        }
+        public AttackDirection AttackDir
+        {
+            get
+            {
+                return (AttackDirection)animator.GetInteger("AttackDir");
+            }
+            set
+            {
+                if (AttackDir != value)
+                    animator.SetInteger("AttackDir", (int)value);
+            }
+        }
+        public bool IsWalking
+        {
+            get
+            {
+                return animator.GetBool("IsWalking");
+            }
+            set
+            {
+                if (IsWalking != value)
+                    animator.SetBool("IsWalking", value);
+            }
+        }
+        public bool IsGrounded
+        {
+            get
+            {
+                return animator.GetBool("IsGrounded");
+            }
+            set
+            {
+                if (IsGrounded != value)
+                    animator.SetBool("IsGrounded", value);
+            }
+        }
 
         private float previousHorizontal;
 
@@ -25,51 +82,75 @@ namespace Assets.Scripts.InputScripts
             rigidBody = GetComponent<Rigidbody2D>();
             collider = GetComponent<Collider>();
 
-            distToGround = collider.bounds.extents.y;
+            IsWalking = false;
+            IsGrounded = true;
+            AttackDir = AttackDirection.Neutral;
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 newVector = transform.localPosition;
-            if (Input.GetAxis("Horizontal") > 0)
+            Vector3 charecterMovement = new Vector3();
+
+            bool isAttacking = Input.GetKey(attackKey) || Input.GetKey(specialKey);
+            if (isAttacking)
             {
-                transform.localRotation = Quaternion.Euler(0, 0, 0);
-                if (Input.GetAxis("Horizontal") >= previousHorizontal)
-                {
-                    newVector.x += runSpeed;
-                }
+                if (Input.GetKey(rightKey) || Input.GetKey(leftKey))
+                    AttackDir = AttackDirection.Side;
+                else if (Input.GetKey(upKey))
+                    AttackDir = AttackDirection.Up;
+                else if (Input.GetKey(downKey))
+                    AttackDir = AttackDirection.Down;
+                else
+                    AttackDir = AttackDirection.Neutral;
+            }
+            else if (Input.GetKey(rightKey))
+            {
+                charecterMovement.x = runSpeed;
+            }
+            else if (Input.GetKey(leftKey))
+            {
+                charecterMovement.x = -runSpeed;
+            }
+            else if (Input.GetKey(upKey) && IsGrounded)
+            {
+                rigidBody.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
             }
 
-            if (Input.GetAxis("Horizontal") < 0)
-            {
-                transform.localRotation = Quaternion.Euler(0, 180, 0);
-                if (Input.GetAxis("Horizontal") <= previousHorizontal)
-                {
-                    newVector.x -= runSpeed;
-                }
-            }
+            if (!isAttacking) AttackDir = AttackDirection.Neutral;
+            if (Input.GetKey(rightKey)) transform.localRotation = Quaternion.Euler(0, 0, 0);
+            else if (Input.GetKey(leftKey)) transform.localRotation = Quaternion.Euler(0, 180, 0);
 
-            if(Input.GetAxis("Vertical") > 0)
-            {
-                if (IsGrounded())
-                {
-                    rigidBody.AddForce(new Vector2(0, 7), ForceMode2D.Impulse);
-                    animator.SetBool("IsGrounded", false);
-                }
-            }
-            if (IsGrounded())
-            {
-                animator.SetBool("IsGrounded", true);
-            }
-            previousHorizontal = Input.GetAxis("Horizontal");
-
-            transform.localPosition = newVector;
+            IsSpecialAttack = Input.GetKey(specialKey);
+            UpdateGrounded();
+            IsWalking = !(charecterMovement.Equals(new Vector3()));
+            transform.localPosition += charecterMovement;
         }
-
-        public bool IsGrounded()
+        void UpdateGrounded()
         {
-            return rigidBody.velocity.y == 0;
+            var floors = GameObject.FindGameObjectsWithTag("floor");
+            foreach (var floor in floors)
+            {
+                var points = floor.GetComponent<PolygonCollider2D>().points;
+                float closestDistance = Vector3.Distance(transform.position, points[0]);
+                foreach (var point in points)
+                    closestDistance = Math.Min(closestDistance,
+                        Vector3.Distance(transform.position, point));
+                if (closestDistance < groundedTolerance)
+                {
+                    IsGrounded = true;
+                    return;
+                }
+            }
+                
+            IsGrounded = false;
         }
+    }
+    public enum AttackDirection : int
+    {
+        Neutral = 0,
+        Up = 1,
+        Side = 2,
+        Down = 3
     }
 }
